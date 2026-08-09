30f827921f73aa6b687144ee67d103828dc28d5e
lehatkako904-droid <lehatkako904@gmail.com>
2026-08-10 01:00:03 +0300

    Delete kurdistan-market directory

--- kurdistan-market/static/script.js
+++
@@ -1,9 +0,0 @@
-function confirmDelete(url) {
-  if (window.confirm("دڵنیایت لە سڕینەوەی ئەم کاڵایە؟")) {
-    const form = document.createElement("form");
-    form.method = "POST";
-    form.action = url;
-    document.body.appendChild(form);
-    form.submit();
-  }
-}
